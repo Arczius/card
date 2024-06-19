73e11e69badb15288a57702cb33c7f22af05ca1f
Jar <jar@jar-desktop.home>
2024-06-19 17:10:06 +0200

    fixed some styling issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,15 @@
     <title>JarBottle's Card</title> 
 </svelte:head>
 
-<div class="w-full md:h-screen flex items-center justify-center">
-    <div class="bg-slate-300/60 w-full md:w-3/4 max-w-4xl rounded-3xl p-4 grid md:grid-cols-2 lg:grid-cols-3 gap-2">
-        <div class="md:col-span-2 lg:col-span-3 border-b-2 border-b-slate-800 border-solid">
+<div class="w-full h-screen flex items-center justify-center px-3 md:px-0">
+    <div class="bg-slate-300/60 w-full md:w-3/4 max-w-4xl rounded-3xl p-4 grid md:grid-cols-2 xl:grid-cols-3 gap-2">
+        <div class="md:col-span-2 xl:col-span-3 border-b-2 border-b-slate-800 border-solid">
             <h1 class="font-bold text-4xl text-slate-800">JarBottle</h1>
         </div>
         <div class="flex justify-center md:justify-normal">
             <img src="/profile_picture.png" alt="" class="rounded max-h-64 max-w-64">
         </div>
-        <div class="lg:col-span-2 w-full md:w-2/3 flex flex-wrap flex-col justify-center">
+        <div class="xl:col-span-2 w-full md:w-2/3 flex flex-wrap flex-col justify-center">
             <div class="grid gap-2">
                 <ul class="grid gap-1">
                     <li>just your average chronically online person</li>
