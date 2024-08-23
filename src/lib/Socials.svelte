e28bea1bc3fe4e9827cb7209c841954b6cda9e3c
Arczius <arczius.development@gmail.com>
2024-08-24 00:13:49 +0200

    restyled the entire page

--- src/lib/Socials.svelte
+++ src/lib/Socials.svelte
@@ -1,47 +1,68 @@
-<h3 class="font-bold">Socials:</h3>
-<ul class="flex gap-2">
+<ul class="flex flex-wrap gap-2">
     {#each socials as social}
-        <li>
-            <a href="{social.link}" target="_blank">
-                <Icon icon="{social.icon}" color="{social.colour}" height="{social.size}" width="{social.size}"/>
+        <li class="w-full grid">
+            <a href="{social.link}" target="_blank" class="flex p-2 bg-slate-200 rounded-xl justify-center text-neutral-800 hover:bg-slate-300 focus:bg-slate-300">
+                <div class="grid grid-cols-3 max-w-32">
+                    <div class="col-span-1/1"><Icon icon="{social.icon}" color="{social.colour}" height="{social.size}" width="{social.size}"/></div>
+                    <div class="col-span-2/3">{social.name}</div>
+                </div>
             </a>
         </li>
-	{/each}
+    {/each}
 </ul>
 
 <script>
     import Icon from '@iconify/svelte';
 
-    let socials = [
+    const socials = [
         {
             icon: "mdi:spotify",
             size: "28",
             colour: "#1DB954",
-            link: "https://open.spotify.com/user/3123lrnctw5kvuxtomksnpokrnbu?si=aa5c74ca481c4787"
+            link: "https://open.spotify.com/user/3123lrnctw5kvuxtomksnpokrnbu?si=aa5c74ca481c4787",
+            name: "Spotify"
         },
         {
             icon: "mdi:steam",
             size: "28",
             colour: "#1b2838",
             link: "https://steamcommunity.com/id/jarbottle",
+            name: "Steam"
         },
         {
             icon: "mdi:github",
             size: "28",
             colour: "#171515",
             link: "https://github.com/Arczius",
+            name: "GitHub"
         },
         {
             icon: "mdi:lastfm",
             size: "28",
             colour: "#D51007",
             link: "https://www.last.fm/user/JarBottle",
+            name: "Last.fm"
         },
         {
             icon: "clarity:cd-dvd-solid",
             size: "28",
             colour: "#000",
             link: "https://www.discogs.com/user/jarbottle/collection",
+            name: "Discogs"
+        },
+        {
+            icon: "simple-icons:trakt",
+            size: "26",
+            colour: "#ED1C24",
+            link: "https://trakt.tv/users/jarbottle",
+            name: "Trakt.tv"
+        },
+        {
+            icon: "simple-icons:myanimelist",
+            size: "26",
+            colour: "#2e51a2",
+            link: "https://myanimelist.net/profile/JarTheBottle",
+            name: "MyAnimeList"
         },
     ]
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
     <title>JarBottle's Card</title> 
 </svelte:head>
 
-<div class="w-full h-screen flex items-center justify-center px-3 md:px-0">
-    <div class="bg-slate-300/60 w-full md:w-3/4 max-w-4xl rounded-3xl p-4 grid md:grid-cols-2 xl:grid-cols-3 gap-2">
+<div class="w-full md:h-screen flex items-center justify-center p-3 md:px-0">
+    <!-- <div class="bg-slate-300/60 w-full md:w-3/4 max-w-4xl rounded-3xl p-4 grid md:grid-cols-2 xl:grid-cols-3 gap-2">
         <div class="md:col-span-2 xl:col-span-3 border-b-2 border-b-slate-800 border-solid">
             <h1 class="font-bold text-4xl text-slate-800">JarBottle</h1>
         </div>
@@ -22,6 +22,34 @@
                 </div>
 
             </div>
+        </div>
+    </div> -->
+    <div class="bg-neutral-800/90 w-full md:w-3/4 max-w-4xl p-4 rounded-3xl grid md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div>
+            <div class="flex gap-2 items-center w-full">
+                <img src="/profile_picture.png" alt="" class="rounded-full max-h-16 max-w-16">
+                <h2 class="font-bold text-4xl text-slate-200">JarBottle</h2>
+            </div>
+            <div class="text-slate-200 p-1">
+                <p>Just your average chronically online person</p>
+                <p>
+                    <b>Likes:</b>
+                </p>
+                <ul class="list-disc pl-4">
+                    <li>Music</li>
+                    <li>Videogames</li>
+                    <li>Coding</li>
+                    <li>Comics</li>
+                    <li>Anime/Manga</li>
+                    <li>Movies</li>
+                </ul>
+                <br>
+                <p>For me it currently is: <span class="text-green-400"><Clock/></span></p>
+            </div>
+        </div>
+        <div class="xl:col-span-2 w-full flex flex-wrap flex-col justify-center">
+            <h3 class="md:hidden text-slate-200 font-bold text-xl pb-1">Socials:</h3>
+            <Socials/>
         </div>
     </div>
 </div>
